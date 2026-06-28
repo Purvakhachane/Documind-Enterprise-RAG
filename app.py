1520630935c3c52312d7b620fc8bae125e4714e8
praveen161622 <praveenvenkat1919@gmail.com>
2026-06-28 21:06:03 +0530

    Implement PDF text extraction using PyPDFLoader

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+from document_processor.pdf_loader import save_pdf, load_pdf
+
 st.set_page_config(
     page_title="Documind Enterprise RAG",
     layout="wide"
@@ -7,13 +9,21 @@ st.set_page_config(
 
 st.title("📄 Documind Enterprise RAG")
 
+# Create the uploader FIRST
 uploaded_file = st.file_uploader(
     "Upload PDF",
     type=["pdf"]
 )
 
-if uploaded_file:
+# Now uploaded_file exists
+if uploaded_file is not None:
 
-    st.success(
-        f"Uploaded: {uploaded_file.name}"
-    )
+    file_path = save_pdf(uploaded_file)
+
+    documents = load_pdf(file_path)
+
+    st.success("PDF Loaded Successfully!")
+
+    st.subheader("Document Preview")
+
+    st.write(documents[0].page_content[:1000])
--- document_processor/pdf_loader.py
+++ document_processor/pdf_loader.py
@@ -1,24 +1,25 @@
+from langchain_community.document_loaders import PyPDFLoader
 import os
 
 def save_pdf(uploaded_file):
 
-    os.makedirs(
-        "uploads",
-        exist_ok=True
-    )
+    os.makedirs("uploads", exist_ok=True)
 
     file_path = os.path.join(
         "uploads",
         uploaded_file.name
     )
 
-    with open(
-        file_path,
-        "wb"
-    ) as f:
-
-        f.write(
-            uploaded_file.getbuffer()
-        )
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
 
     return file_path
+
+
+def load_pdf(file_path):
+
+    loader = PyPDFLoader(file_path)
+
+    documents = loader.load()
+
+    return documents
